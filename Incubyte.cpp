1c3b57abdc25ef815baa5b4b7aa150d1099f24ec
rujulak <115549599+rujulak@users.noreply.github.com>
2023-09-15 12:02:18 +0530

    Update Incubyte.cpp

--- Incubyte.cpp
+++ Incubyte.cpp
@@ -14,5 +14,33 @@ int main()
     cin >> startDir;
 
     Spacecraft spacecraft(startX, startY, startZ, startDir);
+    
+    int n;
+    cout << "Enter Number of Commands: ";
+    cin >> n;
+    vector<char> commands(n);
+    cout << "Enter Commands: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> commands[i];
+    }
+
+    for (char command : commands)
+    {
+        if (command == 'f')
+            spacecraft.moveForward();
+        else if (command == 'b')
+            spacecraft.moveBackward();
+        else if (command == 'l')
+            spacecraft.turnLeft();
+        else if (command == 'r')
+            spacecraft.turnRight();
+        else if (command == 'u')
+            spacecraft.turnUp();
+        else if (command == 'd')
+            spacecraft.turnDown();
+    }
+
+    spacecraft.printCoordinates();
     return 0;
 }
